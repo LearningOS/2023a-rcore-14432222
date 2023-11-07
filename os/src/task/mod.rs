39c27595b83e947f6c591d1859dc245792d1f16d
hook <3012393483@qq.com>
2023-11-07 03:48:52 -0800

    ?

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -46,6 +46,7 @@ pub fn suspend_current_and_run_next() {
     let task_cx_ptr = &mut task_inner.task_cx as *mut TaskContext;
     // Change status to Ready
     task_inner.task_status = TaskStatus::Ready;
+    
     task_inner.stride = task_inner.stride + BIG_STRIDE / task_inner.priority;
     drop(task_inner);
     // ---- release current PCB
